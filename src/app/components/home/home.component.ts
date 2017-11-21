1de708953cee528f776eaf0da8e8c3218505986b
Petter <petterannerwall@gmail.com>
2017-11-22 00:31:25 +0100

    Started testing some stuff

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ElectronService } from 'app/providers/electron.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title = `App works !`;
 
-  constructor() { }
+  constructor(private electronService: ElectronService) {
+
+    const BrowserWindow = this.electronService.Electron.remote.BrowserWindow;
+    const mainWindow = new BrowserWindow({
+      'width': 200,
+      'height': 100,
+      'center': true,
+      'title': 'Main window',
+    });
+
+    const proxy = 'random.se.hma.rocks';
+
+    // mainWindow.webContents.session.setProxy({ pacScript: '', proxyRules: proxy, proxyBypassRules: '' }, function () {
+    //   mainWindow.loadURL('https://whatismyipaddress.com/');
+    // });
+
+    mainWindow.loadURL('https://whatismyipaddress.com/');
+
+  }
 
   ngOnInit() {
   }
--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
 import { ipcRenderer } from 'electron';
+import { BrowserWindow } from 'electron';
 import * as childProcess from 'child_process';
 
 @Injectable()
@@ -10,12 +11,14 @@ export class ElectronService {
 
   ipcRenderer: typeof ipcRenderer;
   childProcess: typeof childProcess;
+  Electron;
 
   constructor() {
     // Conditional imports
     if (this.isElectron()) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
+      this.Electron = window.require('electron');
     }
   }
 
